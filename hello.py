226f92cdb2f4b84e21f6e5af2f2174e188bdbbd9
darai <neguse_amr@hotmail.com>
2019-12-16 00:19:57 +0900

    first commit

--- hello.py
+++ hello.py
@@ -1,1 +1,37 @@
-print('hello')
+import boto3
+import textwrap
+
+s3 = boto3.resource('s3')
+
+# for bucket in s3.buckets.all():
+#     print(bucket.name)
+
+bucket = 'dabo3-sample'
+file = 'test.txt'
+
+obj = s3.Object(bucket, file)
+
+# print(obj.key)
+
+response = obj.get()
+body = response['Body'].read()
+
+# print(type(body))
+
+message = body.decode('utf-8')
+
+message_list = message.splitlines()
+message_list.append('112.4')
+
+for xx in message_list:
+    text = f'''
+
+    あなたの本日の体重は
+    
+    {xx}kg
+    
+    '''
+    print(repr(text))
+    print('-----------')
+    print(repr(textwrap.dedent(text)))
+
---
+++ study.md
@@ -0,0 +1,4 @@
+
+* s3からオブジェクトを取得する
+  * https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Object.get
+  * https://qiita.com/sokutou-metsu/items/5ba7531117224ee5e8af
