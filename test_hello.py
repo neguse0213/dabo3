41268a29e2764d703f1062c3cc8461352e0ba29c
darai <neguse_amr@hotmail.com>
2019-12-20 00:18:17 +0900

    mock試行錯誤中

--- test_hello.py
+++ test_hello.py
@@ -1,17 +1,21 @@
 import unittest
 import hello
 
+from unittest.mock import patch
+
 
 class TestWellnessModoki(unittest.TestCase):
 
     def setUp(self):
         pass
 
-    # WIP
-    def test_showWeight(self):
-        hello.WellnessModoki().show_weight().retur_value = "hoge"
-        actual = hello.WellnessModoki()
-        self.assertEqual("a", "a")
+    @patch('hello.WellnessModoki')
+    def test_showWeight(self, mock_wel):
+        mock_wel.show_weight.return_value = 'fuga'
+        mo = hello.WellnessModoki
+        no = mo.show_weight
+
+        self.assertEqual(no, 'test')
 
 
 if __name__ == "__main__":
